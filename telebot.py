dc05b6d39bc422b866aea58d676c8092a7ebe231
Alexandt <dgv17022005@gmail.com>
2025-02-23 23:12:14 +0500

    publish photo to Telegram channel

--- telebot.py
+++ telebot.py
@@ -3,11 +3,13 @@ import telegram
 from dotenv import load_dotenv
 
 
-
 dotenv_path = os.path.join('secrets.env')
 load_dotenv(dotenv_path=dotenv_path)
+
 api_bot = os.environ['API_TELEBOT']
 
 bot = telegram.Bot(token=api_bot)
 
-bot.send_message(chat_id='@imagensa', text="test")
+chat_id = '@imagensa'
+
+bot.send_photo(chat_id=chat_id, photo=open('images/spacex_2.jpg', 'rb'))
