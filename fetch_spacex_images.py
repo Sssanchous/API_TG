048eff02b83364345f6a31cb0c5a50cdb7c2ea34
Alexandt <dgv17022005@gmail.com>
2025-02-28 15:30:32 +0500

    update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -14,18 +14,15 @@ def fetch_spacex_last_launch(id_last_launch):
     response.raise_for_status()
 
     launches = response.json()
-    data = launches["links"]["flickr"]["original"]
+    image_url = launches["links"]["flickr"]["original"]
 
-    for id, link in enumerate(data, start=1):
+    for link_id, link in enumerate(image_url, start=1):
         download_image(f"spacex_{id}.jpg", link, IMAGES_DIR)
 
-def main():
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Скачивание фотографий SpaceX")
     parser.add_argument("id_last_launch", help="ID запуска SpaceX (по умолчанию – последний запуск)", nargs="?", default='latest')
 
     args = parser.parse_args()
     fetch_spacex_last_launch(args.id_last_launch)
-
-
-if __name__ == "__main__":
-    main()
