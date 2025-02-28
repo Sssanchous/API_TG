21b6b1620ef98252d5c7bbd88b8715507b809a80
Alexandt <dgv17022005@gmail.com>
2025-02-28 15:30:17 +0500

    update fetch_nasa_photo.py

--- fetch_nasa_photo.py
+++ fetch_nasa_photo.py
@@ -2,7 +2,7 @@ import os
 import argparse
 import requests
 from dotenv import load_dotenv
-from utils import download_image, check_extension
+from utils import download_image, get_extension
 
 API_NASA = os.getenv("API_NASA")
 IMAGES_DIR = os.path.join(os.getcwd(), "images")
@@ -11,13 +11,8 @@ load_dotenv()
 os.makedirs(IMAGES_DIR, exist_ok=True)
 
 def fetch_nasa_photo(count):
-
-    dotenv_path = os.path.join('secrets.env')
-    load_dotenv(dotenv_path=dotenv_path)
-    api_key = os.environ['API_NASA']
-
     url = f'https://api.nasa.gov/planetary/apod?api_key={api_key}'
-    payload ={
+    payload = {
         "api_key": api_key,
         "count": count
     }
@@ -27,19 +22,20 @@ def fetch_nasa_photo(count):
 
     photos = response.json()
 
-    for id, photo in enumerate(photos, start=1):
+    for photo_id, photo in enumerate(photos, start=1):
         if 'url' in photo:
             link = photo['url']
-            extension = check_extension(link)
+            extension = get_extension(link)
             download_image(f"Nasa_{id}{extension}", link, IMAGES_DIR)
 
 
-def main():
+if __name__ == "__main__":
+    dotenv_path = os.path.join('secrets.env')
+    load_dotenv(dotenv_path=dotenv_path)
+    api_key = os.environ['API_NASA']
+
     parser = argparse.ArgumentParser(description="Скачивание фотографий NASA")
     parser.add_argument("count", type=int, help="Количество фотографий для скачивания", default=1)
 
     args = parser.parse_args()
     fetch_nasa_photo(args.count)
-
-if __name__ == "__main__":
-    main()
